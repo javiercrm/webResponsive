ae6d3902ab49bb4a3b3197dd9671ba8d703d0047
javiercrm <43179062+javiercrm@users.noreply.github.com>
2018-11-23 19:47:56 +0100

    Add files via upload

--- js/botonSubir.js
+++ js/botonSubir.js
@@ -1,4 +1,9 @@
 /* Mostramos el boton una vez bajemos 350 px la pantalla*/
+
+$( document ).ready(function() {
+    $('.botonSubir').fadeOut(0);
+});
+
 $(window).scroll(function(){
     if ($(this).scrollTop() > 350) {
         $('.botonSubir').fadeIn();
